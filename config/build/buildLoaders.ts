2731da7e6c01d82336a26aef42ebbc8ae76282df
Anatoly <anatolykostarev@gmail.com>
2022-09-14 01:34:25 +0300

    Add scss webpack loader

--- config/build/buildLoaders.ts
+++ config/build/buildLoaders.ts
@@ -2,13 +2,27 @@ import webpack from "webpack";
 
 export function buildLoaders (): webpack.RuleSetRule[] {
 
+    const cssLoader = {
+        test: /\.s[ac]ss$/i,
+        use: [
+            // Creates `style` nodes from JS strings
+            "style-loader",
+            // Translates CSS into CommonJS
+            "css-loader",
+            // Compiles Sass to CSS
+            "sass-loader",
+        ],
+    }
+
+    // Если не используем TS, нужен babel-loader
     const typescriptLoader = {
-            test: /\.tsx?$/,
-            use: 'ts-loader',
-            exclude: /node_modules/,
-        }
+        test: /\.tsx?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+    }
 
     return [
         typescriptLoader,
+        cssLoader,
     ]
 }
