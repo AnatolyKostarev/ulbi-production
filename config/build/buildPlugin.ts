026b678736f1de7918f29a98805d1610d5f9036c
Anatoly <anatolykostarev@gmail.com>
2022-09-15 02:06:13 +0300

    Set react module css (#5)
    * Add webpack mini-css-extract-plugin
    
    * Add MiniCssExtractPlugin.loader
    
    * Add MiniCssExtractPlugin.loader
    
    * Add options param
    
    * Add module css
    
    * Create App component
    
    * Refactor code
    
    * Add global styles

--- config/build/buildPlugin.ts
+++ config/build/buildPlugin.ts
@@ -2,8 +2,9 @@ import HtmlWebpackPlugin from "html-webpack-plugin";
 import path from "path";
 import webpack from "webpack";
 import {BuildOptions} from "./types/config";
+import MiniCssExtractPlugin from "mini-css-extract-plugin";
 
-export function buildPlugin ({paths}: BuildOptions): webpack.WebpackPluginInstance[] {
+export function buildPlugin({paths}: BuildOptions): webpack.WebpackPluginInstance[] {
 
 
     return [
@@ -11,5 +12,9 @@ export function buildPlugin ({paths}: BuildOptions): webpack.WebpackPluginInstan
             template: paths.html,
         }),
         new webpack.ProgressPlugin(),
+        new MiniCssExtractPlugin({
+            filename: "css/[name].[contenthash:8].css",
+            chunkFilename:  "css/[name].[contenthash:8].css",
+        }),
     ]
 }
