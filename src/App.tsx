ea531b488c85a60f48fec46ce564cf7dc5ccdfa7
Anatoly <anatolykostarev@gmail.com>
2022-09-15 23:31:22 +0300

    Add react router, lazy loading for components (#6)
    * Add property for page reload
    
    * Create MainPage, AbouyPage components
    
    * Add code splitting, lazy component to load chuncks
    
    * Add Suspense for lazy loading of pages
    
    * Add Router
    
    * Add react-router-dom

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,23 @@
-import React from 'react';
-import {Counter} from "./components/Counter";
+import React, {Suspense} from 'react';
+import {Link, Route, Routes} from 'react-router-dom';
 import './index.scss';
+import {MainPageAsync} from "./pages/MainPage/MainPageAsync";
+import {AboutPageAsync} from "./pages/AboutPage/AboutPageAsync";
+
 
 const App = () => {
     return (
         <div className="app">
-         <h2>Application</h2>
-            <Counter/>
+            <Link to={'/'}>На Главную</Link>
+            <Link to={'/about'}>О сайте</Link>
+            <Suspense fallback={<div>Loading...</div>}>
+
+                <Routes>
+                    <Route path={'/'} element={<MainPageAsync/>}/>
+                    <Route path={'/about'} element={<AboutPageAsync/>}/>
+                </Routes>
+            </Suspense>
+
         </div>
     );
 };
