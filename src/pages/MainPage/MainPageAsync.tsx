a396095bfe31ce34335d24f5c48be04dd05ebeed
Anatoly <anatolykostarev@gmail.com>
2022-09-17 23:08:43 +0300

    Replace component acc. FSD

--- src/pages/MainPage/MainPageAsync.tsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-export const MainPageAsync = React.lazy(() => new Promise((resolve) => {
-    // @ts-ignore
-    //так в реальных проектах не делать. Используем только для курса.
-    setTimeout(()=>resolve(import('./MainPage')), 1500)
-}));
