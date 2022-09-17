0f752aada70c568847967a161b50e26ea125d5e4
Anatoly <anatolykostarev@gmail.com>
2022-09-17 18:57:35 +0300

    Refactor className for App with classNames function

--- src/App.tsx
+++ src/App.tsx
@@ -3,17 +3,17 @@ import {Link, Route, Routes} from 'react-router-dom';
 import {MainPageAsync} from "./pages/MainPage/MainPageAsync";
 import {AboutPageAsync} from "./pages/AboutPage/AboutPageAsync";
 import {useTheme} from "./theme/useTheme";
+import {classNames} from "./helpers/classNames/classNames";
 import './styles/index.scss';
 
 
 const App = () => {
     const {theme, toggleTheme} = useTheme();
     return (
-        <div className={`app ${theme}`}>
+        <div className={classNames("app", {}, [theme])}>
             <Link to={'/'}>На Главную</Link>
             <Link to={'/about'}>О сайте</Link>
             <Suspense fallback={<div>Loading...</div>}>
-
                 <Routes>
                     <Route path={'/'} element={<MainPageAsync/>}/>
                     <Route path={'/about'} element={<AboutPageAsync/>}/>
