4cc91092dd54d78a17bcd79aa594cd125cf0e0d5
Anatoly <anatolykostarev@gmail.com>
2022-09-17 23:07:38 +0300

    Replace component according FSD

--- src/pages/AboutPage/AboutPageAsync.tsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-export const AboutPageAsync = React.lazy(() => new Promise((resolve) => {
-    // @ts-ignore
-    //так в реальных проектах не делать. Используем только для курса.
-    setTimeout(()=>resolve(import('./AboutPage')), 1500)
-}));
