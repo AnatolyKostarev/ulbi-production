f5a1f97f2d4488608f8eeee4f1ebb7842c73cde9
Anatoly <anatolykostarev@gmail.com>
2022-09-21 02:24:44 +0300

    Add Navbar component, AppLink component (#10)
    * Create NavBar component for widgets slice acc FSD
    
    * Create Applink Component for shared slice acc FSD
    
    * Refactor code
    
    * Add var for global styles
    
    * Fix colors
    
    * Replace Links, add Navbar component

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,15 +1,15 @@
-import { Link } from "react-router-dom";
 import { AppRouter } from "./providers/router";
 import { useTheme } from "./providers/ThemeProvider";
 import { classNames } from "shared/lib/classNames/classNames";
+import { Navbar } from "widgets/Navbar";
+
 import "./styles/index.scss";
 
 const App = () => {
   const { theme, toggleTheme } = useTheme();
   return (
     <div className={classNames("app", {}, [theme])}>
-      <Link to={"/"}>На Главную</Link>
-      <Link to={"/about"}>О сайте</Link>
+      <Navbar />
       <AppRouter />
       <button onClick={toggleTheme}>TOGGLE</button>
     </div>
---
+++ src/widgets/Navbar/ui/Navbar.tsx
@@ -0,0 +1,21 @@
+import {classNames} from "shared/lib/classNames/classNames";
+import {AppLink, AppLinkTheme} from "shared/ui/AppLink/AppLink";
+import cls from "./Navbar.module.scss";
+
+interface NavbarProps {
+  className?: string;
+}
+
+export const Navbar = ({ className }: NavbarProps) => {
+    return (
+    <div className={classNames(cls.Navbar, {}, [className])}>
+        <div className={classNames(cls.links)}>
+            <AppLink theme={AppLinkTheme.SECONDARY} to={"/"}>На Главную</AppLink>
+            <AppLink theme={AppLinkTheme.SECONDARY} to={"/about"}>О сайте</AppLink>
+        </div>
+
+    </div>
+  );
+};
+
+
