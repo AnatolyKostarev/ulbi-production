415da0c18e7e3b3a8cab26fa77ef3a0cc36c94f1
Anatoly <anatolykostarev@gmail.com>
2022-09-22 01:48:06 +0300

    Replace theme switcher

--- src/widgets/Navbar/ui/Navbar.tsx
+++ src/widgets/Navbar/ui/Navbar.tsx
@@ -1,7 +1,6 @@
 import {classNames} from "shared/lib/classNames/classNames";
 import {AppLink, AppLinkTheme} from "shared/ui/AppLink/AppLink";
 import cls from "./Navbar.module.scss";
-import {ThemeSwitcher} from "shared/ui/ThemeSwitcher";
 
 interface NavbarProps {
     className?: string;
@@ -10,7 +9,6 @@ interface NavbarProps {
 export const Navbar = ({className}: NavbarProps) => {
     return (
         <div className={classNames(cls.Navbar, {}, [className])}>
-            <ThemeSwitcher/>
             <div className={classNames(cls.links)}>
                 <AppLink theme={AppLinkTheme.SECONDARY} to={"/"}>На Главную</AppLink>
                 <AppLink theme={AppLinkTheme.SECONDARY} to={"/about"}>О сайте</AppLink>
