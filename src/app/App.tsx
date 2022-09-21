7e099fe515d6a12587d012fabdf7e7e4ae1f86b9
Anatoly <anatolykostarev@gmail.com>
2022-09-22 00:02:26 +0300

    Replace button into Button component in share folder

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,19 +1,17 @@
-import { AppRouter } from "./providers/router";
-import { useTheme } from "./providers/ThemeProvider";
-import { classNames } from "shared/lib/classNames/classNames";
-import { Navbar } from "widgets/Navbar";
-
+import {AppRouter} from "./providers/router";
+import {useTheme} from "./providers/ThemeProvider";
+import {classNames} from "shared/lib/classNames/classNames";
+import {Navbar} from "widgets/Navbar";
 import "./styles/index.scss";
 
 const App = () => {
-  const { theme, toggleTheme } = useTheme();
-  return (
-    <div className={classNames("app", {}, [theme])}>
-      <Navbar />
-      <AppRouter />
-      <button onClick={toggleTheme}>TOGGLE</button>
-    </div>
-  );
+    const {theme} = useTheme();
+    return (
+        <div className={classNames("app", {}, [theme])}>
+            <Navbar/>
+            <AppRouter/>
+        </div>
+    );
 };
 
 export default App;
