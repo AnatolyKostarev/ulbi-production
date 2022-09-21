2928a42b6cffb992c0cf93ab1f320cf38e984a41
Anatoly <anatolykostarev@gmail.com>
2022-09-22 00:07:35 +0300

    Add .svg -loader,  file loader, create Button UI Kit (#11)
    * Add svg, file loaders to import icons like svg, pmng etc
    
    * Add Theme for Button Component
    
    * Add module for TS to recognize icons extension in import statements
    
    * Replace button into Button component in share folder
    
    * Add icons
    
    * Create Button component in share/ui folder
    
    * Create Theme switcher component
    
    * Reformat coge with Prettier
    
    * Refactor with Prettier
    
    * Add packeges for svg, png, jpg, etc

--- src/widgets/Navbar/ui/Navbar.tsx
+++ src/widgets/Navbar/ui/Navbar.tsx
@@ -1,21 +1,23 @@
 import {classNames} from "shared/lib/classNames/classNames";
 import {AppLink, AppLinkTheme} from "shared/ui/AppLink/AppLink";
 import cls from "./Navbar.module.scss";
+import {ThemeSwitcher} from "shared/ui/ThemeSwitcher";
 
 interface NavbarProps {
-  className?: string;
+    className?: string;
 }
 
-export const Navbar = ({ className }: NavbarProps) => {
+export const Navbar = ({className}: NavbarProps) => {
     return (
-    <div className={classNames(cls.Navbar, {}, [className])}>
-        <div className={classNames(cls.links)}>
-            <AppLink theme={AppLinkTheme.SECONDARY} to={"/"}>На Главную</AppLink>
-            <AppLink theme={AppLinkTheme.SECONDARY} to={"/about"}>О сайте</AppLink>
-        </div>
+        <div className={classNames(cls.Navbar, {}, [className])}>
+            <ThemeSwitcher/>
+            <div className={classNames(cls.links)}>
+                <AppLink theme={AppLinkTheme.SECONDARY} to={"/"}>На Главную</AppLink>
+                <AppLink theme={AppLinkTheme.SECONDARY} to={"/about"}>О сайте</AppLink>
+            </div>
 
-    </div>
-  );
+        </div>
+    );
 };
 
 
