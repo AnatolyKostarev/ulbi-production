345f82ff7cfc6b614ebd27c8e435a8080619be32
Anatoly <anatolykostarev@gmail.com>
2022-09-22 01:49:42 +0300

    Add Sidebar (#12)
    * Add styles, className
    
    * Add Sidebar size vars
    
    * Add global styles
    
    * Add Sidebar component into App
    
    * Replace theme switcher
    
    * Create Sidebar component & styles
    
    * Add public API

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -2,6 +2,7 @@ import {AppRouter} from "./providers/router";
 import {useTheme} from "./providers/ThemeProvider";
 import {classNames} from "shared/lib/classNames/classNames";
 import {Navbar} from "widgets/Navbar";
+import {Sidebar} from "widgets/Sidebar";
 import "./styles/index.scss";
 
 const App = () => {
@@ -9,7 +10,10 @@ const App = () => {
     return (
         <div className={classNames("app", {}, [theme])}>
             <Navbar/>
-            <AppRouter/>
+            <div className="content-page">
+                <Sidebar/>
+                <AppRouter/>
+            </div>
         </div>
     );
 };
--- src/app/providers/router/ui/AppRouter.tsx
+++ src/app/providers/router/ui/AppRouter.tsx
@@ -1,21 +1,25 @@
-import React, { Suspense } from "react";
-import { Routes, Route } from "react-router-dom";
-import { routerConfig } from "shared/config/routerConfig/routerConfig";
+import React, {Suspense} from "react";
+import {Routes, Route} from "react-router-dom";
+import {routerConfig} from "shared/config/routerConfig/routerConfig";
 
 export default function AppRouter() {
-  return (
-    <Suspense fallback={<div>Loading...</div>}>
-      <Routes>
-        {Object.values(routerConfig).map(({ element, path }) => (
-          <Route
-            key={path}
-            path={path}
-            element={
-              <Suspense fallback={<div>Loading...</div>}>{element}</Suspense>
-            }
-          />
-        ))}
-      </Routes>
-    </Suspense>
-  );
+    return (
+        <Suspense fallback={<div>Loading...</div>}>
+            <Routes>
+                {Object.values(routerConfig).map(({element, path}) => (
+                    <Route
+                        key={path}
+                        path={path}
+                        element={
+                            <Suspense fallback={<div>Loading...</div>}>
+                                <div className="page-wrapper">
+                                    {element}
+                                </div>
+                            </Suspense>
+                        }
+                    />
+                ))}
+            </Routes>
+        </Suspense>
+    );
 }
