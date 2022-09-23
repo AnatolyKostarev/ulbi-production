127de148d0251d18280ee6f15ab6049e07fc90b5
Anatoly <anatolykostarev@gmail.com>
2022-09-23 18:44:12 +0300

    Add func ti translate pages react i18next

--- src/pages/MainPage/ui/MainPage.tsx
+++ src/pages/MainPage/ui/MainPage.tsx
@@ -1,11 +1,14 @@
-import React from 'react';
+import React from "react";
+import { useTranslation } from "react-i18next";
 
 const MainPage = () => {
-    return (
-        <div>
-            <h1>MAIN PAGE</h1>
-        </div>
-    );
+  const { t } = useTranslation("main");
+
+  return (
+    <div>
+       <h1>{t("Главная страница")}</h1>
+    </div>
+  );
 };
 
 export default MainPage;
