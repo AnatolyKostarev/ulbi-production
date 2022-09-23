0e31a7033f9c57481d0f403fb78613a4b93f69df
Anatoly <anatolykostarev@gmail.com>
2022-09-23 18:43:32 +0300

    Refactor

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,21 +1,26 @@
-import {AppRouter} from "./providers/router";
-import {useTheme} from "./providers/ThemeProvider";
-import {classNames} from "shared/lib/classNames/classNames";
-import {Navbar} from "widgets/Navbar";
-import {Sidebar} from "widgets/Sidebar";
+import { AppRouter } from "./providers/router";
+import { useTheme } from "./providers/ThemeProvider";
+import { classNames } from "shared/lib/classNames/classNames";
+import { Navbar } from "widgets/Navbar";
+import { Sidebar } from "widgets/Sidebar";
+import { Suspense } from "react";
 import "./styles/index.scss";
 
+
+
 const App = () => {
-    const {theme} = useTheme();
-    return (
-        <div className={classNames("app", {}, [theme])}>
-            <Navbar/>
-            <div className="content-page">
-                <Sidebar/>
-                <AppRouter/>
-            </div>
+  const { theme } = useTheme();
+  return (
+    <div className={classNames("app", {}, [theme])}>
+      <Suspense fallback="">
+        <Navbar />
+        <div className="content-page">
+          <Sidebar />
+          <AppRouter />
         </div>
-    );
+      </Suspense>
+    </div>
+  );
 };
 
 export default App;
