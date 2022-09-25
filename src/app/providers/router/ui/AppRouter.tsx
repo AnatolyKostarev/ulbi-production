c9fe238f3df11a6dce42e3f686a6850eed7bb3a9
Anatoly <anatolykostarev@gmail.com>
2022-09-25 15:00:29 +0300

    Refactor code

--- src/app/providers/router/ui/AppRouter.tsx
+++ src/app/providers/router/ui/AppRouter.tsx
@@ -1,23 +1,27 @@
 import React, { Suspense } from 'react';
-import { Routes, Route } from 'react-router-dom';
-import { routerConfig } from 'shared/config/routerConfig/routerConfig';
+import { Route, Routes } from 'react-router-dom';
+import { AboutPage } from 'pages/AboutPage';
+import { MainPage } from 'pages/MainPage';
+import { routeConfig } from 'shared/config/routeConfig/routeConfig';
 
-export default function AppRouter() {
-  return (
+const AppRouter = () => (
     <Suspense fallback={<div>Loading...</div>}>
-      <Routes>
-        {Object.values(routerConfig).map(({ element, path }) => (
-          <Route
-            key={path}
-            path={path}
-            element={
-              <Suspense fallback={<div>Loading...</div>}>
-                <div className='page-wrapper'>{element}</div>
-              </Suspense>
-            }
-          />
-        ))}
-      </Routes>
+        <Routes>
+            {Object.values(routeConfig).map(({ element, path }) => (
+                <Route
+                    key={path}
+                    path={path}
+                    element={(
+                        <Suspense fallback={<div>Loading...</div>}>
+                            <div className="page-wrapper">
+                                {element}
+                            </div>
+                        </Suspense>
+                    )}
+                />
+            ))}
+        </Routes>
     </Suspense>
-  );
-}
+);
+
+export default AppRouter;
