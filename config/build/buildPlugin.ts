0168dd8d7cbe25e59f1dfbd4992035403a68ef23
Anatoly <anatolykostarev@gmail.com>
2022-09-25 13:06:03 +0300

    Refactor with ESlint

--- config/build/buildPlugin.ts
+++ config/build/buildPlugin.ts
@@ -1,26 +1,26 @@
-import HtmlWebpackPlugin from "html-webpack-plugin";
-import webpack from "webpack";
-import { BuildOptions } from "./types/config";
-import MiniCssExtractPlugin from "mini-css-extract-plugin";
+import HtmlWebpackPlugin from 'html-webpack-plugin';
+import webpack from 'webpack';
+import MiniCssExtractPlugin from 'mini-css-extract-plugin';
+import { BuildOptions } from './types/config';
 
 export function buildPlugin({
-  paths,
-  isDev,
+    paths,
+    isDev,
 }: BuildOptions): webpack.WebpackPluginInstance[] {
-  return [
-    new HtmlWebpackPlugin({
-      template: paths.html,
-    }),
-    new webpack.ProgressPlugin(),
-    new MiniCssExtractPlugin({
-      filename: "css/[name].[contenthash:8].css",
-      chunkFilename: "css/[name].[contenthash:8].css",
-    }),
+    return [
+        new HtmlWebpackPlugin({
+            template: paths.html,
+        }),
+        new webpack.ProgressPlugin(),
+        new MiniCssExtractPlugin({
+            chunkFilename: 'css/[name].[contenthash:8].css',
+            filename: 'css/[name].[contenthash:8].css',
+        }),
 
-    new webpack.DefinePlugin({
-      __IS_DEV__: JSON.stringify(isDev),
-    }),
+        new webpack.DefinePlugin({
+            __IS_DEV__: JSON.stringify(isDev),
+        }),
 
-    new webpack.HotModuleReplacementPlugin(),
-  ];
+        new webpack.HotModuleReplacementPlugin(),
+    ];
 }
