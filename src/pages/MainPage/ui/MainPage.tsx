8d2d69dfcc0accd98e43c4392c2c2e02d9c670e9
Anatoly <anatolykostarev@gmail.com>
2022-09-25 12:58:51 +0300

    Refactor with ESlint

--- src/pages/MainPage/ui/MainPage.tsx
+++ src/pages/MainPage/ui/MainPage.tsx
@@ -2,13 +2,13 @@ import React from 'react';
 import { useTranslation } from 'react-i18next';
 
 const MainPage = () => {
-  const { t } = useTranslation('main');
+    const { t } = useTranslation('main');
 
-  return (
-    <div>
-      <h1>{t('Главная страница')}</h1>
-    </div>
-  );
+    return (
+        <div>
+            <h1>{t('Главная страница')}</h1>
+        </div>
+    );
 };
 
 export default MainPage;
