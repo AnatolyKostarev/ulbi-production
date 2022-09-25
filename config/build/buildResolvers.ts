55450e35fb5f7dc4b6bba75f8271a60175e357fa
Anatoly <anatolykostarev@gmail.com>
2022-09-25 14:59:37 +0300

    Refactor code

--- config/build/buildResolvers.ts
+++ config/build/buildResolvers.ts
@@ -3,12 +3,10 @@ import { BuildOptions } from './types/config';
 
 export function buildResolvers(options: BuildOptions): ResolveOptions {
     return {
-        alias: {},
-        extensions: ['.tsx',
-            '.ts',
-            '.js'],
-        mainFiles: ['index'],
-        modules: [options.paths.src, 'node_modules'],
+        extensions: ['.tsx', '.ts', '.js'],
         preferAbsolute: true,
+        modules: [options.paths.src, 'node_modules'],
+        mainFiles: ['index'],
+        alias: {},
     };
 }
