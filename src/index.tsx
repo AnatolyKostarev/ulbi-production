58f2b2db3251143de3464be725d8fff65e7aa422
Anatoly <anatolykostarev@gmail.com>
2022-09-25 15:02:44 +0300

    Refactor code

--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,15 @@
 import { render } from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
-import './shared/config/i18n/i18n';
-import App from './app/App';
 import { ThemeProvider } from 'app/providers/ThemeProvider';
+import App from './app/App';
+
+import './shared/config/i18n/i18n';
 
 render(
-  <BrowserRouter>
-    <ThemeProvider>
-      <App />
-    </ThemeProvider>
-  </BrowserRouter>,
-  document.getElementById('root')
+    <BrowserRouter>
+        <ThemeProvider>
+            <App />
+        </ThemeProvider>
+    </BrowserRouter>,
+    document.getElementById('root'),
 );
--- src/widgets/Navbar/ui/Navbar.tsx
+++ src/widgets/Navbar/ui/Navbar.tsx
@@ -3,27 +3,18 @@ import { AppLink, AppLinkTheme } from 'shared/ui/AppLink/AppLink';
 import cls from './Navbar.module.scss';
 
 interface NavbarProps {
-  className?: string;
+    className?: string;
 }
 
-export const Navbar = ({ className }: NavbarProps) => {
-  return (
+export const Navbar = ({ className }: NavbarProps) => (
     <div className={classNames(cls.Navbar, {}, [className])}>
-      <div className={classNames(cls.links)}>
-        <AppLink
-          theme={AppLinkTheme.SECONDARY}
-          to={'/'}
-        >
-          На Главную
-        </AppLink>
-
-        <AppLink
-          theme={AppLinkTheme.SECONDARY}
-          to={'/about'}
-        >
-          О сайте
-        </AppLink>
-      </div>
+        <div className={cls.links}>
+            <AppLink theme={AppLinkTheme.SECONDARY} to="/" className={cls.mainLink}>
+                Главная
+            </AppLink>
+            <AppLink theme={AppLinkTheme.RED} to="/about">
+                О сайте
+            </AppLink>
+        </div>
     </div>
-  );
-};
+);
