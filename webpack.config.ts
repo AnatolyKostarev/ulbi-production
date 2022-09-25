87635dd0ee071f2a01343b5d89c0acff491f5d41
Anatoly <anatolykostarev@gmail.com>
2022-09-25 15:03:16 +0300

    Refactor code

--- webpack.config.ts
+++ webpack.config.ts
@@ -1,12 +1,12 @@
-import path from 'path';
 import webpack from 'webpack';
+import path from 'path';
 import { buildWebpackConfig } from './config/build/buildWebpackConfig';
 import { BuildEnv, BuildPaths } from './config/build/types/config';
 
 export default (env: BuildEnv) => {
     const paths: BuildPaths = {
-        build: path.resolve(__dirname, 'build'),
         entry: path.resolve(__dirname, 'src', 'index.tsx'),
+        build: path.resolve(__dirname, 'build'),
         html: path.resolve(__dirname, 'public', 'index.html'),
         src: path.resolve(__dirname, 'src'),
     };
@@ -17,10 +17,11 @@ export default (env: BuildEnv) => {
     const isDev = mode === 'development';
 
     const config: webpack.Configuration = buildWebpackConfig({
-        isDev,
         mode,
         paths,
+        isDev,
         port: PORT,
     });
+
     return config;
 };
