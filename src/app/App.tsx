abc747909eca7bee132f51e6513f53d5340935df
Anatoly <anatolykostarev@gmail.com>
2022-09-25 13:04:46 +0300

    Refactor with ESlint

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -7,18 +7,18 @@ import { AppRouter } from './providers/router';
 import './styles/index.scss';
 
 function App() {
-  const { theme } = useTheme();
-  return (
-    <div className={classNames('app', {}, [theme])}>
-      <Suspense fallback=''>
-        <Navbar />
-        <div className='content-page'>
-          <Sidebar />
-          <AppRouter />
+    const { theme } = useTheme();
+    return (
+        <div className={classNames('app', {}, [theme])}>
+            <Suspense fallback="">
+                <Navbar />
+                <div className="content-page">
+                    <Sidebar />
+                    <AppRouter />
+                </div>
+            </Suspense>
         </div>
-      </Suspense>
-    </div>
-  );
+    );
 }
 
 export default App;
