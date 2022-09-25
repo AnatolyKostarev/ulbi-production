7a98a884b27de0b8bbd8bab0f455a7bc5cdd5964
Anatoly <anatolykostarev@gmail.com>
2022-09-25 15:02:23 +0300

    Refactor code

--- src/shared/config/i18n/i18n.ts
+++ src/shared/config/i18n/i18n.ts
@@ -1,5 +1,6 @@
 import i18n from 'i18next';
 import { initReactI18next } from 'react-i18next';
+
 import Backend from 'i18next-http-backend';
 import LanguageDetector from 'i18next-browser-languagedetector';
 
@@ -8,15 +9,16 @@ i18n
     .use(LanguageDetector)
     .use(initReactI18next)
     .init({
+        fallbackLng: 'en',
+        debug: __IS_DEV__,
+
+        interpolation: {
+            escapeValue: false, // not needed for react as it escapes by default
+        },
+
         backend: {
             loadPath: '/locales/{{lng}}/{{ns}}.json',
         },
-        debug: __IS_DEV__,
-        fallbackLng: 'ru',
-        interpolation: {
-            escapeValue: false,
-        },
-
     });
 
 export default i18n;
