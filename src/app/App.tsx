2b22793f0241a7b2b99da342eb210b5a74dad2e6
Anatoly <anatolykostarev@gmail.com>
2022-09-25 15:01:25 +0300

    Refactor code

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,13 +1,14 @@
+import React, { Suspense } from 'react';
+import './styles/index.scss';
 import { classNames } from 'shared/lib/classNames/classNames';
+import { useTheme } from 'app/providers/ThemeProvider';
+import { AppRouter } from 'app/providers/router';
 import { Navbar } from 'widgets/Navbar';
 import { Sidebar } from 'widgets/Sidebar';
-import { Suspense } from 'react';
-import { useTheme } from './providers/ThemeProvider';
-import { AppRouter } from './providers/router';
-import './styles/index.scss';
 
 function App() {
     const { theme } = useTheme();
+
     return (
         <div className={classNames('app', {}, [theme])}>
             <Suspense fallback="">
