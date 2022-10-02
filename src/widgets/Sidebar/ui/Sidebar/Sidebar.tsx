8f02a2cc10eb3de8beca31d2df3c34bfd91e264e
Anatoly <anatolykostarev@gmail.com>
2022-10-02 19:44:31 +0300

    Add translation for Sidebar toggle button

--- src/widgets/Sidebar/ui/Sidebar/Sidebar.tsx
+++ src/widgets/Sidebar/ui/Sidebar/Sidebar.tsx
@@ -3,6 +3,7 @@ import { useState } from 'react';
 import { ThemeSwitcher } from 'shared/ui/ThemeSwitcher';
 import { LangSwitcher } from 'shared/ui/LangSwitcher/LangSwitcher';
 import { Button } from 'shared/ui/Button/Button';
+import { useTranslation } from 'react-i18next';
 import cls from './Sidebar.module.scss';
 
 interface SidebarProps {
@@ -16,6 +17,7 @@ export const Sidebar = ({ className }: SidebarProps) => {
         setCollapsed((prev) => !prev);
     };
 
+    const { t } = useTranslation();
     return (
         <div
             data-testid="sidebar"
@@ -25,7 +27,7 @@ export const Sidebar = ({ className }: SidebarProps) => {
                 data-testid="sidebar-toggle"
                 onClick={onToggle}
             >
-                toggle
+                {t('toggle')}
             </Button>
             <div className={cls.switchers}>
                 <ThemeSwitcher />
