36ca0b25df1215aa1eca98053542242b3cb02d35
Anatoly <anatolykostarev@gmail.com>
2022-10-09 00:26:21 +0300

    Fix modal global styles

--- src/shared/ui/Modal/Modal.tsx
+++ src/shared/ui/Modal/Modal.tsx
@@ -1,26 +1,27 @@
 import { classNames } from 'shared/lib/classNames/classNames';
 import React, {
-    ReactNode, useCallback, useEffect, useRef, useState,
+    ReactNode,
+    useCallback,
+    useEffect,
+    useRef,
+    useState,
 } from 'react';
 import { Portal } from 'shared/ui/Portal/Portal';
 import { useTheme } from 'app/providers/ThemeProvider';
 import cls from './Modal.module.scss';
 
 interface ModalProps {
-    className?: string;
-    children?: ReactNode;
-    isOpen?: boolean;
-    onClose?: () => void;
+  className?: string
+  children?: ReactNode
+  isOpen?: boolean
+  onClose?: () => void
 }
 
 const ANIMATION_DELAY = 300;
 
 export const Modal = (props: ModalProps) => {
     const {
-        className,
-        children,
-        isOpen,
-        onClose,
+        className, children, isOpen, onClose,
     } = props;
 
     const [isClosing, setIsClosing] = useState(false);
@@ -38,11 +39,14 @@ export const Modal = (props: ModalProps) => {
     }, [onClose]);
 
     // Новые ссылки!!!
-    const onKeyDown = useCallback((e: KeyboardEvent) => {
-        if (e.key === 'Escape') {
-            closeHandler();
-        }
-    }, [closeHandler]);
+    const onKeyDown = useCallback(
+        (e: KeyboardEvent) => {
+            if (e.key === 'Escape') {
+                closeHandler();
+            }
+        },
+        [closeHandler],
+    );
 
     const onContentClick = (e: React.MouseEvent) => {
         e.stopPropagation();
@@ -66,12 +70,11 @@ export const Modal = (props: ModalProps) => {
 
     return (
         <Portal>
-            <div className={classNames(cls.Modal, mods, [className, theme])}>
+            <div
+                className={classNames(cls.Modal, mods, [className, theme, 'app_modal'])}
+            >
                 <div className={cls.overlay} onClick={closeHandler}>
-                    <div
-                        className={cls.content}
-                        onClick={onContentClick}
-                    >
+                    <div className={cls.content} onClick={onContentClick}>
                         {children}
                     </div>
                 </div>
