5e75b158b9abfa3f68727a5778dc2cc782c81520
Anatoly <anatolykostarev@gmail.com>
2022-10-18 18:02:29 +0300

    Add custom input, lazy auth modal (#40)

--- src/index.tsx
+++ src/index.tsx
@@ -3,9 +3,9 @@ import { BrowserRouter } from 'react-router-dom';
 import { ThemeProvider } from 'app/providers/ThemeProvider';
 import { StoreProvider } from 'app/providers/StoreProvider';
 import App from './app/App';
-import 'app/styles/index.scss';
 import './shared/config/i18n/i18n';
 import { ErrorBoundary } from './app/providers/ErrorBoundary';
+import 'app/styles/index.scss';
 
 render(
     <StoreProvider>
--- src/pages/MainPage/ui/MainPage.tsx
+++ src/pages/MainPage/ui/MainPage.tsx
@@ -1,14 +1,11 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useTranslation } from 'react-i18next';
+import { Input } from 'shared/ui/Input/Input';
 
 const MainPage = () => {
     const { t } = useTranslation();
 
-    return (
-        <div>
-            {t('Главная страница')}
-        </div>
-    );
+    return <div>{t('Главная страница')}</div>;
 };
 
 export default MainPage;
