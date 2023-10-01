489bfd20e24b1fcc7a8687e1c7f5b84ebb251fd5
Anatoly <anatolykostarev@gmail.com>
2023-10-02 01:16:00 +0300

    Add login module (#44)

--- src/app/providers/StoreProvider/config/store.ts
+++ src/app/providers/StoreProvider/config/store.ts
@@ -2,11 +2,13 @@ import { configureStore, ReducersMapObject } from '@reduxjs/toolkit';
 import { counterReducer } from 'entities/Counter';
 import { userReducer } from 'entities/User';
 import { StateSchema } from './StateSchema';
+import { loginReducer } from 'features/AuthByUsername/model/slice/loginSlice';
 
 export function createReduxStore(initialState?: StateSchema) {
     const rootReduser: ReducersMapObject<StateSchema> = {
         user: userReducer,
         counter: counterReducer,
+        loginForm: loginReducer,
     };
     return configureStore<StateSchema>({
         reducer: rootReduser,
